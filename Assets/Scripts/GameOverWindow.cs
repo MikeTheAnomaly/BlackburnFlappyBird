d776c0696b5af481dfdb5a005173e87e59c2ae93
MikeTheAnomaly <methoff@gmail.com>
2021-08-24 16:28:35 -0500

    Done the Game

---
+++ Assets/Scripts/GameOverWindow.cs
@@ -0,0 +1,81 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+using TMPro;
+
+public class GameOverWindow : MonoBehaviour {
+
+    private Text scoreText;
+    private Text highscoreText;
+    public TMP_InputField PlayerName;
+
+    private bool waitforName = false;
+
+    private void Awake() {
+        scoreText = transform.Find("scoreText").GetComponent<Text>();
+        highscoreText = transform.Find("highscoreText").GetComponent<Text>();
+        
+        transform.Find("retryBtn").GetComponent<Button_UI>().ClickFunc = () => { Loader.Load(Loader.Scene.GameScene); };
+        transform.Find("retryBtn").GetComponent<Button_UI>().AddButtonSounds();
+        
+        transform.Find("mainMenuBtn").GetComponent<Button_UI>().ClickFunc = () => { if (!waitforName || PlayerName.text != "") { Loader.Load(Loader.Scene.MainMenu); } };
+        transform.Find("mainMenuBtn").GetComponent<Button_UI>().AddButtonSounds();
+        PlayerName.onEndEdit.AddListener((string s) => { Score.TrySetNewName(s); if (s == "resetcheat") { Score.ResetHighscore(); } });
+        transform.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
+
+    private void Start() {
+        Bird.GetInstance().OnDied += Bird_OnDied;
+        Hide();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Home) && Input.GetKeyDown(KeyCode.Z)) {
+            // Retry
+            Loader.Load(Loader.Scene.GameScene);
+            Score.ResetHighscore();
+        }
+    }
+
+    private void Bird_OnDied(object sender, System.EventArgs e) {
+        scoreText.text = Level.GetInstance().GetPipesPassedCount().ToString();
+
+        if (Level.GetInstance().GetPipesPassedCount() >= Score.GetHighscore()) {
+            // New Highscore!
+            highscoreText.text = "NEW HIGHSCORE";
+            transform.Find("retryBtn").gameObject.SetActive(false);
+            PlayerName.gameObject.SetActive(true);
+            waitforName = true;
+        } else {
+            waitforName = false;
+            highscoreText.text = "HIGHSCORE: " + Score.GetHighscore() + " By: " + Score.GetName();
+            transform.Find("retryBtn").gameObject.SetActive(true);
+            PlayerName.gameObject.SetActive(false);
+        }
+
+        Show();
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,76 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Score {
+
+    public static void Start() {
+        //ResetHighscore();
+        if (Bird.GetInstance() != null)
+        {
+            Bird.GetInstance().OnDied += Bird_OnDied;
+        }
+    }
+
+    private static void Bird_OnDied(object sender, System.EventArgs e) {
+        TrySetNewHighscore(Level.GetInstance().GetPipesPassedCount());
+    }
+
+    public static int GetHighscore() {
+        return PlayerPrefs.GetInt("highscore");
+    }
+
+    public static string GetName()
+    {
+        return PlayerPrefs.GetString("name");
+    }
+
+    public static bool TrySetNewHighscore(int score) {
+        int currentHighscore = GetHighscore();
+        if (score > currentHighscore) {
+            // New Highscore
+            PlayerPrefs.SetInt("highscore", score);
+            PlayerPrefs.Save();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static bool TrySetNewName(string name)
+    {
+        int currentHighscore = GetHighscore();
+        if (name != GetName())
+        {
+            // New Highscore
+            PlayerPrefs.SetString("name", name);
+            PlayerPrefs.SetString("debug", PlayerPrefs.GetString("debug") + " /" + name + "," + currentHighscore);
+            Debug.Log(PlayerPrefs.GetString("debug"));
+            PlayerPrefs.Save();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void ResetHighscore() {
+        PlayerPrefs.SetInt("highscore", 0);
+        PlayerPrefs.SetString("name", "");
+        PlayerPrefs.SetString("debug", "debug:");
+        PlayerPrefs.Save();
+    }
+}
